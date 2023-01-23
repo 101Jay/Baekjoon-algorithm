ec3e54b2feaccc461be73cb3db01a7c8bf3c53bd
Jay code <kjk8569@naver.com>
2023-01-24 00:36:25 +0900

    pro-11650 solved with Arrays.sort

--- src/com/practice/problem/pro_11650/Main.java
+++ src/com/practice/problem/pro_11650/Main.java
@@ -1,73 +1,47 @@
 package com.practice.problem.pro_11650;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
 
     /**
      * 1. 입력될 좌표의 개수를 받는다
      * 2. 그 숫자만큼 반복문을 돌려 각 좌표들을 입력 받는다.
-     * 3. 각 좌표를 순서대로 배열에 넣되, 앞 순서의 숫자보다 x 좌표가 음의 영역에 가깝다면, 즉 x좌표의 수가 더 작다면 이전 수를 현재 수로 바꾸고 기존의 자리를 차지한다.
-     * 시간초과의 이유?
+     * 3. Arrays 객체의 sort 메서드를 활용한다.
+     * ** Arrays 객체의 sort 메서드는 Quicksort를 사용하고 있으며, 평균 nlogn의 시간복잡도를 갖는다.
      * */
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int inputNumber = Integer.parseInt(br.readLine());
 
-        /** 추후 비교를 위한 초기값 설정 */
-        int[][] resultArr = new int[inputNumber + 1][2];
-
-        /** 첫 번째 비교 대상의 위치의 좌표(현재 좌표보다 하나 전)*/
-        int cmpPosX = resultArr[0][0] = -100001;
-        int cmpPosY = resultArr[0][1] = -100001;
+        int[][] resultArr = new int[inputNumber][2];
 
         String[] tempArr;
 
-        int curPos;
-
-        for(int i = 1; i <= inputNumber; i++){
-
-            curPos = i;
+        /** 개수만큼 2차원 배열로 입력 받기 */
+        for(int i = 0; i < inputNumber; i++){
 
             tempArr = br.readLine().split(" ");
 
-            while(Integer.parseInt(tempArr[0]) <= cmpPosX){
-
-                if(Integer.parseInt(tempArr[0]) == cmpPosX){
-
-                    /** Y좌표를 기준으로 비교 -> 크거나 같으면 앞으로 당길 필요가 없음 : break*/
-                    if(Integer.parseInt(tempArr[1]) >= cmpPosY){
-                        break;
-                    }
-                }
-
-                /** 현재 위치와 비교 위치를 한 칸 앞당김 -> bubble sort */
-                curPos--;
-                cmpPosX = resultArr[curPos - 1][0];
-                cmpPosY = resultArr[curPos - 1][1];
-            }
-
-            /** 현재 저장 위치로부터 한 칸씩 밀어서 저장 */
-            for(int k = i; k > curPos; k--){
-
-                resultArr[k][0] = resultArr[k-1][0];
-                resultArr[k][1] = resultArr[k-1][1];
-            }
-
-            /** 앞서 비교를 통해 정해진 위치에 새로운 좌표를 저장 */
-            resultArr[curPos][0] = Integer.parseInt(tempArr[0]);
-            resultArr[curPos][1] = Integer.parseInt(tempArr[1]);
-
-            /** 이후 비교를 위한 비교 위치 변경 */
-            cmpPosX = resultArr[i][0];
-            cmpPosY = resultArr[i][1];
+            resultArr[i][0] = Integer.parseInt(tempArr[0]);
+            resultArr[i][1] = Integer.parseInt(tempArr[1]);
 
         }
 
+        /** Arrays 객체의 sort 메소드를 활용하여 정렬 */
+        Arrays.sort(resultArr, (o1, o2) -> {
+            if(o1[0] == o2[0]){
+                return Integer.compare(o1[1], o2[1]);
+            } else{
+                return Integer.compare(o1[0], o2[0]);
+            }
+        });
+
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for(int j = 1; j < resultArr.length; j++){
+        for(int j = 0; j < resultArr.length; j++){
             bw.write(resultArr[j][0] + " " + resultArr[j][1] + "\n");
         }
         bw.close();
---
+++ src/com/practice/problem/pro_11650/Main_bubble.java
@@ -0,0 +1,75 @@
+package com.practice.problem.pro_11650;
+
+import java.io.*;
+
+public class Main_bubble {
+
+    /**
+     * 1. 입력될 좌표의 개수를 받는다
+     * 2. 그 숫자만큼 반복문을 돌려 각 좌표들을 입력 받는다.
+     * 3. 각 좌표를 순서대로 배열에 넣되, 앞 순서의 숫자보다 x 좌표가 음의 영역에 가깝다면, 즉 x좌표의 수가 더 작다면 이전 수를 현재 수로 바꾸고 기존의 자리를 차지한다.
+     * ** 해당 클래스는 Bubble sort를 활용하여 직접 구현했고, 시간 복잡도가 높아 적절한 해결책은 아니었다.
+     * */
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int inputNumber = Integer.parseInt(br.readLine());
+
+        /** 추후 비교를 위한 초기값 설정 */
+        int[][] resultArr = new int[inputNumber + 1][2];
+
+        /** 첫 번째 비교 대상의 위치의 좌표(현재 좌표보다 하나 전)*/
+        int cmpPosX = resultArr[0][0] = -100001;
+        int cmpPosY = resultArr[0][1] = -100001;
+
+        String[] tempArr;
+
+        int curPos;
+
+        for(int i = 1; i <= inputNumber; i++){
+
+            curPos = i;
+
+            tempArr = br.readLine().split(" ");
+
+            while(Integer.parseInt(tempArr[0]) <= cmpPosX){
+
+                if(Integer.parseInt(tempArr[0]) == cmpPosX){
+
+                    /** Y좌표를 기준으로 비교 -> 크거나 같으면 앞으로 당길 필요가 없음 : break*/
+                    if(Integer.parseInt(tempArr[1]) >= cmpPosY){
+                        break;
+                    }
+                }
+
+                /** 현재 위치와 비교 위치를 한 칸 앞당김 -> bubble sort */
+                curPos--;
+                cmpPosX = resultArr[curPos - 1][0];
+                cmpPosY = resultArr[curPos - 1][1];
+            }
+
+            /** 현재 저장 위치로부터 한 칸씩 밀어서 저장 */
+            for(int k = i; k > curPos; k--){
+
+                resultArr[k][0] = resultArr[k-1][0];
+                resultArr[k][1] = resultArr[k-1][1];
+            }
+
+            /** 앞서 비교를 통해 정해진 위치에 새로운 좌표를 저장 */
+            resultArr[curPos][0] = Integer.parseInt(tempArr[0]);
+            resultArr[curPos][1] = Integer.parseInt(tempArr[1]);
+
+            /** 이후 비교를 위한 비교 위치 변경 */
+            cmpPosX = resultArr[i][0];
+            cmpPosY = resultArr[i][1];
+
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for(int j = 1; j < resultArr.length; j++){
+            bw.write(resultArr[j][0] + " " + resultArr[j][1] + "\n");
+        }
+        bw.close();
+    }
+}
